475dee4b996249a72e35f4e3cf7f15a20aeb1880
icyeza <sesameicyeza@gmail.com>
2026-05-25 11:30:16 +0200

    executable

--- supervised_learning/nlp_metrics/0-uni_blue.py
+++ supervised_learning/nlp_metrics/0-uni_blue.py
@@ -1,49 +1,35 @@
 #!/usr/bin/env python3
-"""BLEU Score"""
+'''
+    calculates the unigram BLEU score
+'''
+
 
 import numpy as np
 
 
 def uni_bleu(references, sentence):
-    """
-    sentence_blue
-    Args:
-        references: list of reference translations
-        sentence: list containing the model proposed sentence
-    Returns: unigram BLEU score
-    """
+    '''
+        calculates the unigram BLEU score
+        for a sentence
+    '''
+    sentence_length = len(sentence)
+    references_length = []
+    words = {}
 
-    sen = list(set(sentence))
-    count_dict = {}
+    for translation in references:
+        references_length.append(len(translation))
+        for word in translation:
+            if word in sentence and word not in words.keys():
+                words[word] = 1
 
-    # Count appearances
-    for reference in references:
-        for word in reference:
-            if word in sen:
-                if word not in count_dict.keys():
-                    count_dict[word] = reference.count(word)
-                else:
-                    new = reference.count(word)
-                    old = count_dict[word]
-                    count_dict[word] = max(new, old)
+    total = sum(words.values())
+    index = np.argmin([abs(len(i) - sentence_length) for i in references])
+    best_match = len(references[index])
 
-    # Clipping
-    len_sen = len(sentence)
-    list_references = []
-    for reference in references:
-        len_ren = len(reference)
-        list_references.append(((abs(len_ren - len_sen)), len_ren))
-
-    # Precision
-    reference_len = sorted(list_references, key=lambda x: x[0])
-    reference_len = reference_len[0][1]
-
-    # Penalty
-    if len_sen > reference_len:
-        bp = 1
+    if sentence_length > best_match:
+        BLEU = 1
     else:
-        bp = np.exp(1 - (float(reference_len) / len_sen))
+        BLEU = np.exp(1 - float(best_match) / float(sentence_length))
+    BLEU_score = BLEU * np.exp(np.log(total / sentence_length))
 
-    bleu_score = bp * np.exp(np.log(sum(count_dict.values()) / len_sen))
-
-    return bleu_score
+    return BLEU_score
